4012345da0545ef9d00a9703688e1c8de68019d4
Adit0507 <adi05072002@gmail.com>
2024-08-23 14:39:27 +0530

    passed :zap:

--- cache_test.go
+++ cache_test.go
@@ -7,6 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCache_MaxSize(t*testing.T) {
+	t.Parallel()
+
+	c := cache.New[int, int](3, time.Minute)
+	c.Upsert(1,1)
+	c.Upsert(2,2)
+	c.Upsert(3,3)
+	
+	got, found := c.Read(1)
+	assert.True(t, found)
+	assert.Equal(t, 1, got)
+
+	c.Upsert(1, 10)
+	c.Upsert(4,4)
+
+	got, found = c.Read(2)
+	assert.False(t, found)
+	assert.Equal(t, 0, got)
+}
+
 func TestCache_TTL(t *testing.T) {
 	t.Parallel()
 	c := cache.New[string, string](5, time.Millisecond*100)
